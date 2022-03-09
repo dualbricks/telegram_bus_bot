073e6c6dd1f88b26a073fe7daa1631393830fdf8
dualbricks <wenyuemawen@gmail.com>
2022-03-09 23:32:26 +0800

    Speed optimisation through storing Bus Stop data in file instead of API calling

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const { Telegraf } = require('telegraf')
-const bot = new Telegraf(process.env.bot_key)
+const bot = new Telegraf(process.env.bot_key);
 const bus_command = require('./commands/bus');
-
+const bus_code_command = require('./commands/busStop');
 
 //bot start command
 bot.start( async (ctx) => {
-  console.log('working')
+  console.log('working');
   ctx.reply("Welcome");
   bot.telegram.sendMessage(ctx.chat.id, 'Can we access your location?', requestLocationKeyboard);
 })
@@ -50,7 +50,7 @@ bot.on('message', async (ctx)=> {
       ctx.reply(text).catch();
     }
     else {
-      ctx.reply("No information please double check Bus Code")
+      ctx.reply("No information please double check Bus Code");
     }
   }
 })
